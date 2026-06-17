f0a9befb6a01f7fe11f31492ed32ff6f348424dc
anastasia <anastasia.perfilova@maxbill.com>
2026-06-17 14:03:47 +0300

    feat: add API types and ForexTariff model

---
+++ src/api/types.ts
@@ -0,0 +1,43 @@
+export type ApiValue<T> = { $: T };
+
+export type ApiTariffDetail = {
+  name: ApiValue<string>;
+  value: ApiValue<string>;
+};
+
+export type ApiTariffPrice = {
+  cost: ApiValue<string>;
+  currency: ApiValue<string>;
+  period: ApiValue<string>;
+};
+
+export type ApiTariffElement = {
+  id: ApiValue<string>;
+  title: ApiValue<string>;
+  title_tag?: ApiValue<string>;
+  datacenter: {
+    id: ApiValue<string>;
+    value: ApiValue<string>;
+  };
+  detail: ApiTariffDetail[];
+  prices: {
+    price: ApiTariffPrice[];
+  };
+};
+
+export type ForexTariffsApiResponse = {
+  doc: {
+    list: Array<{
+      elem: ApiTariffElement[];
+    }>;
+  };
+};
+
+export type ForexTariff = {
+  id: string;
+  title: string;
+  datacenterId: number;
+  datacenterName: string;
+  specs: { name: string; value: string }[];
+  prices: { period: number; cost: number }[];
+};
